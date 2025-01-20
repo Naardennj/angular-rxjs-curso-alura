28408ba8458870753f1ed2a608fde98a1993d73c
Naardennj <gabriel.angelis787@gmail.com>
2025-01-20 10:21:56 -0300

    Utilizar a arquitetura de componentes de apresentação e componentes inteligentes Lidar com a biblioteca RxJS Aplicar o padrão Observer

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class ListaLivrosComponent {
   listaLivros: [];
+  campoBusca: string = '';
 
-  constructor() {}
+  constructor(private service: LivroService) {}
+
+  buscarLivros() {
+    this.service
+      .buscar(this.campoBusca)
+      .subscribe((retornoAPI) => console.log(retornoAPI),(error) => console.log(error));
+  }
 }

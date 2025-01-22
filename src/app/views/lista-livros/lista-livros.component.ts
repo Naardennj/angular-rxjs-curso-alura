ac0d575aefeb693836b738610040d16a38e5d2bd
Naardennj <gabriel.angelis787@gmail.com>
2025-01-22 11:26:26 -0300

    Capturar erros com os operadores catchError e throwError Utilizar o operador EMPTY Mostrar mensagens de erro para a pessoa usuária

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { livroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -13,8 +23,23 @@ const PAUSA = 300;
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) {}
+
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((resultado) => (this.livrosResultado = resultado)),
+    catchError((error) => {
+      console.log(error);
+      return of();
+    })
+  );
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
@@ -23,7 +48,17 @@ export class ListaLivrosComponent {
     distinctUntilChanged(),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
     tap(() => console.log('Fluxo final')),
-    map((items) => this.livrosResultadoParaLivros(items))
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((error) => {
+      console.log(error);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro = 'Ocorreu um erro. Recarregue a aplicação!')
+          )
+      );
+    })
   );
 
   livrosResultadoParaLivros(items: Item[]): livroVolumeInfo[] {
